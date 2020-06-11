d6dc2e5c64c901bc24a819da1bb5d101b69f5171
Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-06-11 12:05:38 +0300

    added text

--- src/by/epamtc/zhurava/task02/Main.java
+++ src/by/epamtc/zhurava/task02/Main.java
@@ -3,8 +3,20 @@ package by.epamtc.zhurava.task02;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String text = "А вы ноктюрн сыграть могли бы на флейте водосточных труб?";
+		
+		divideEvenAndUnevenSymbols(text);
+	}
 
+	
+	/**
+	 * Четные и нечетные символы разделить по разным строкам.
+	 * @param text
+	 */
+	private static void divideEvenAndUnevenSymbols(String text) {
+		StringBuilder unevenSymbols = new StringBuilder();
+		StringBuilder evenSymbols = new StringBuilder();
+		
 	}
 
 }
